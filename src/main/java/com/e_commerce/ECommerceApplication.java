db2e359239d6e63762df74d7f1aec0ccc61440ef
soufiane ait talb <soufianeaittalb00@gmail.com>
2023-11-18 18:55:56 +0100

    Change name

--- src/main/java/com/e_commerce/ECommerceApplication.java
+++ src/main/java/com/e_commerce/ECommerceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ECommerce {
+public class ECommerceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ECommerce.class, args);
+		SpringApplication.run(ECommerceApplication.class, args);
 	}
 
 }
